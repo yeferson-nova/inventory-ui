f0206186ae5597b71416e5d531162a8df6ce1317
Yeferson Nova <yeferson_nova@outlook.com>
2024-08-30 02:31:11 -0500

    creacion inicial, componentes dashboard, home, manejo base de rutas

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './pages/dashboard.component';
+import { HomeComponent } from './component/home/home.component';
+import { RouterModule } from '@angular/router';
+
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    HomeComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class DashboardModule { }
